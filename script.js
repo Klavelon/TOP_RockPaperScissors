7631ecc99aac14ad381051c724e25a08076b2108
Klavelon <mortson@hotmail.co.uk>
2022-03-01 20:11:22 +0000

    EStablished input structure and made single input round decisions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Test");
+
+
+function ComputerPlay(){
+    const computerSelection = ["Rock", "Paper", "Scissors"];
+    return computerSelection[Math.floor(Math.random() * 3)];
+}
+
+let computerChoice = ComputerPlay();
+console.log(computerChoice);
+
+let playerChoice;
+
+playerChoice = prompt("Enter your choice of Rock, Paper or Scissors!");
+
+playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+
+
+
+console.log(playerChoice);
+
+if (playerChoice == "Rock" && computerChoice == "Scissors"){
+    console.log("You Win!")
+} else if (playerChoice == "Rock" && computerChoice == "Paper"){
+    console.log("You Lose!");
+} else if (playerChoice == "Rock" && computerChoice == "Rock"){
+    console.log("TIE!");
+}
+else {
+    console.log("Something went dreadfully wrong");
+}
