ed95025ced09ca0deeb744582636322cfb37ab26
Klavelon <mortson@hotmail.co.uk>
2022-03-01 20:47:13 +0000

    Completed game!

--- script.js
+++ script.js
@@ -5,26 +5,116 @@ function ComputerPlay(){
     return computerSelection[Math.floor(Math.random() * 3)];
 }
 
+
+function playRound(){
+
 let computerChoice = ComputerPlay();
-console.log(computerChoice);
-
 let playerChoice;
-
 playerChoice = prompt("Enter your choice of Rock, Paper or Scissors!");
-
 playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+console.log("Computer plays " +computerChoice);
 
+function versus(){
+    if (playerChoice == "Rock"){
+    
+        if (computerChoice == "Scissors"){
+    
+            console.log("You Win!")
+            return "w";
+    
+        } else if (computerChoice == "Paper"){
+    
+            console.log("You Lose!");
+            return "l";
+    
+        } else if (computerChoice == "Rock"){
+    
+            console.log("TIE!");
+            return "t";
+        }
+    
+        else {
+    
+            console.log("Something went dreadfully wrong");
+    
+        }
+    
+    } else if (playerChoice == "Paper") {
+    
+        if (computerChoice == "Rock") {
+    
+            console.log("You Win!");
+            return "w";
+    
+        } else if (computerChoice == "Paper"){
+    
+            console.log("TIE!");
+            return "t";
+    
+        } else if (computerChoice == "Scissors"){
+    
+            console.log("You Lose!");
+            return "l";
+        }
+    
+        else {
+    
+            console.log("Something went dreadfully wrong");
+    
+        }
+    
+    } else if (playerChoice == "Scissors"){
+    
+        if (computerChoice == "Rock"){
+    
+            console.log("You Lose!");
+            return "l";
+    
+        } else if (computerChoice == "Paper"){
+    
+            console.log("You Win!");
+            return "w";
+    
+        } else if (computerChoice == "Scissors"){
+    
+            console.log("TIE!");
+            return "t";
+    
+        }
+    
+        else {
+    
+            console.log("Something went dreadfully wrong");
+    
+        }
+    }
+    }
 
+let score = versus();
 
-console.log(playerChoice);
+if (score === "w"){
+    playerScore++;
+} else if (score === "l"){
+    computerScore++;
+} else if (score === "t"){
 
-if (playerChoice == "Rock" && computerChoice == "Scissors"){
-    console.log("You Win!")
-} else if (playerChoice == "Rock" && computerChoice == "Paper"){
-    console.log("You Lose!");
-} else if (playerChoice == "Rock" && computerChoice == "Rock"){
-    console.log("TIE!");
+} else {console.log("What happened?"); }
+
+console.log("Player Score = " +playerScore);
+console.log("Computer Score = " +computerScore);
 }
-else {
-    console.log("Something went dreadfully wrong");
-}
+
+let computerScore = 0;
+let playerScore = 0;
+
+for (let i = 0; i < 5; i++) {
+    playRound();
+ }
+
+ if (playerScore > computerScore){
+     console.log("YOU ARE THE CHAMPION!");
+ } else if (playerScore = computerScore){
+     console.log("OVERALL TIE!");
+ } else {
+     console.log("YOU LOSE SIR. GOOD DAY.");
+ }
